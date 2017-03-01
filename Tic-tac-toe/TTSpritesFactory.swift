3505e280a634676e784e0b822b00eac8147776eb
DPlatov <dplatov@MacBook-Pro-5.local>
2017-03-01 10:22:36 +0200

    Convert to current swift syntax

---
+++ Tic-tac-toe/TTSpritesFactory.swift
@@ -0,0 +1,16 @@
+//
+//  TTSpritesFactory.swift
+//  Tic-tac-toe
+//
+//  Created by DPlatov on 5/12/16.
+//  Copyright © 2016 dplatov. All rights reserved.
+//
+
+import UIKit
+
+class TTSpritesFactory: NSObject {
+    static func sritesArray(_ fromImage:UIImage?) -> [UIImage] {
+        
+        return [];
+    }
+}
